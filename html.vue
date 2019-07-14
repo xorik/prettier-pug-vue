bb0c22f394acadb60355b2e043606d71b5e533e3
 Andrey Smelov <xor29a@bk.ru>
2019-07-14 18:20:45 +0300

    Demo

---
+++ html.vue
@@ -0,0 +1,3 @@
+<template>
+  <div><a class="asdas asda" href="sdfdsdfsdfdsfskfjdlkfjsdlkjfdsklfjads;lkfjskl;fjaklfjadf" title="asada asdsad asd adas"></a></div>
+</template>
---
+++ pug.vue
@@ -0,0 +1,4 @@
+<template lang="pug">
+  div
+    a.asdas.asda(href="sdfdsdfsdfdsfskfjdlkfjsdlkjfdsklfjads;lkfjskl;fjaklfjadf" title="asada asdsad asd adas")
+</template>
